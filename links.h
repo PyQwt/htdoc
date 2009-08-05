bc7584a01b1d54da5c8cdf36cc384ad90c8da824
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2009-08-05 05:41:12 +0000

    Add PDF documentation. Remove numeric and numarray.

--- links.h
+++ links.h
@@ -1,7 +1,8 @@
 <!-- -*- HTML -*- -->
 
 <h3>Documentation</h3>
-<li><a href="./doc5/index.html">PyQwt5 Manual</a>
+<li><a href="./doc5/index.html">PyQwt5 Manual (html)</a>
+<li><a href="./doc5/PyQwt.pdf">PyQwt5 Manual (pdf)</a>
 <li><a href="./doc4/pyqwt.html">PyQwt4 Manual</a>
 <li><a href="./doc3d/pyqwt3d.html">PyQwt3D Manual</a>
 <h3>Development Tools</h3>
@@ -14,10 +15,7 @@
 <li><a href="http://www.riverbankcomputing.com/software/sip/intro">SIP</a>
 <h3>Indispensable Tools</h3>
 <li><a href="http://numpy.scipy.org">NumPy</a>
-<li><a href="http://www.stsci.edu/resources/software_hardware/numarray">numarray</a>
-<li><a href="http://numeric.scipy.org">Numeric</a>
 <li><a href="http://www.scipy.org">SciPy</a>
-<li><a href="http://ht2html.sourceforge.net">ht2html</a>
 <h3>Recommended Tools</h3>
 <li><a href="http://www.die-offenbachs.de/eric/index.html">Eric</a>
 <li><a href="http://plasma-gate.weizmann.ac.il/Grace/">Grace</a>
